2d14ac167ee559078f8c4d9c15d622bad508821e
Thibault Malbranche <malbranche.thibault@gmail.com>
2020-10-21 23:19:40 +0200

    feat(init): created the repo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,45 @@
-import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import UseAnimatedValue from '@brigad/use-animated-value';
+import React, { useState } from 'react';
+import { StyleSheet, View, Animated, Button, Easing, Text } from 'react-native';
+import useAnimatedValue from '@brigad/use-animated-value';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [random, setRandom] = useState(0);
 
-  React.useEffect(() => {
-    UseAnimatedValue.multiply(3, 7).then(setResult);
-  }, []);
+  const offset = useAnimatedValue(random, { useNativeDriver: true });
+  const offset2 = useAnimatedValue(random, { duration: 1000 });
+  const offset3 = useAnimatedValue(random, {
+    easing: Easing.elastic(2),
+    duration: 1000,
+  });
+  const color = useAnimatedValue(random > 0 ? 'red' : 'blue');
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Animated.Text
+        style={{ transform: [{ translateX: offset }], padding: 10 }}
+      >
+        {`I use the native driver!`}
+      </Animated.Text>
+      <Animated.Text
+        style={{ transform: [{ translateX: offset2 }], padding: 10 }}
+      >
+        {`I have a custom duration!`}
+      </Animated.Text>
+      <Animated.Text
+        style={{ transform: [{ translateX: offset3 }], padding: 10 }}
+      >
+        {`I have a custom easing!`}
+      </Animated.Text>
+      <Animated.Text style={{ color, padding: 10 }}>
+        {`I can animate colors!`}
+      </Animated.Text>
+      <Button
+        title="Move text"
+        onPress={() => {
+          setRandom(Math.floor((Math.random() - 0.5) * 200));
+        }}
+      />
+      <Text>Current Value {random}</Text>
     </View>
   );
 }
@@ -21,5 +49,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ccc',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,73 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
+import {
+  useState,
+  useRef,
+  useCallback,
+  useLayoutEffect,
+  useEffect,
+} from 'react';
+
+import { Animated, Easing } from 'react-native';
+
+const usePrevious = <ValueType extends any>(value: ValueType) => {
+  const ref = useRef<ValueType | undefined>();
+
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
 };
+
+type Options<T> = {
+  useNativeDriver?: boolean;
+  duration?: number;
+  easing?: ((value: number) => number) | undefined;
+  onAnimateEnd?: (newValue: T, finished: boolean) => void;
+};
+
+export const useAnimatedValue = <T extends string | number>(
+  value: T,
+  {
+    useNativeDriver = false,
+    duration = 200,
+    easing = Easing.out(Easing.quad),
+    onAnimateEnd,
+  }: Options<T> = {}
+) => {
+  const [currentValue, setCurrentValue] = useState(value);
+  const prevProp = usePrevious(value);
+  const animatedValue = useRef(new Animated.Value(0)).current;
+  const animationTimingRef = useRef<Animated.CompositeAnimation | null>(null);
+
+  const animate = useCallback(
+    (newProp: T) => {
+      animationTimingRef.current?.stop();
+
+      animatedValue.setValue(0);
+      animationTimingRef.current = Animated.timing(animatedValue, {
+        duration,
+        easing,
+        toValue: 1,
+        useNativeDriver,
+      });
+      animationTimingRef.current.start((cb) => {
+        onAnimateEnd?.(newProp, cb.finished);
+        setCurrentValue(newProp);
+      });
+    },
+    [animatedValue, duration, easing, onAnimateEnd, useNativeDriver]
+  );
+
+  useLayoutEffect(() => {
+    if (prevProp !== null && value !== prevProp) {
+      animate(value);
+    }
+  }, [prevProp, animate, value]);
+
+  return animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [currentValue, value] as string[] | number[],
+  });
+};
+
+export default useAnimatedValue;
